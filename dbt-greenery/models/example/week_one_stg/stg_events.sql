6801fd21387942c4126ed374fb244a09fbc836f7
Pat McDonald <patmcdonald@me.com>
2021-11-21 16:20:03 +0000

    write file

---
+++ dbt-greenery/models/example/week_one_stg/stg_events.sql
@@ -0,0 +1,20 @@
+{{
+  config(
+    materialized='table',
+    unique_key='event_id'
+}}
+
+with events_source as (
+    select * from{{  source('tutorial', 'events') }}
+)
+
+select
+    id
+    , event_id
+    , session_id
+    , user_id
+    , event_type
+    , page_url
+    , created_at    as created_at_utc
+    
+from events_source
